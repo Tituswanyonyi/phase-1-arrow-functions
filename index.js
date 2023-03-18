f9f862e83fb326b2f609c07acb452bf8682aa561
titus <titus.unavailable@student.moringaschool.com>
2023-03-18 11:09:21 +0300

    Assignment complete

---
+++ index.js
@@ -0,0 +1,9 @@
+/* function expression */
+const foo = function (){
+    return 'bar'
+}
+
+/* arrow functions */
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+const multiply = (x,y) => x * y;
